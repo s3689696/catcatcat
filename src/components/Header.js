ef5321e29b2b80c6b40f9bef1c5285fc553c78f8
Guy <s3689696@student.rmit.edu.au>
2018-08-25 23:50:35 +1000

    Added icons to home and folder button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faFolder } from '@fortawesome/free-solid-svg-icons';
 
 export class Header extends React.Component {
     render() {
         return (
             <div className="Header">
                 <div className="Header-buttons">
-                    <button>Home</button>
-                    <button>Folders</button>
+                    <button><FontAwesomeIcon icon={faHome}/></button>
+                    <button><FontAwesomeIcon icon={faFolder}/></button>
                 </div>
                 <div className="Header-title">
                     <h1>CatCatCat</h1>
